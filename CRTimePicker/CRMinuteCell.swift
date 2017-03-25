81ad2174f6b719bc0a35d5416aaf143d7773ec5b
cruz <cruzdiary@gmail.com>
2017-03-25 16:22:29 +0900

    feat(timepicker): Add Interaction

---
+++ CRTimePicker/CRMinuteCell.swift
@@ -0,0 +1,25 @@
+//
+//  CRMinuteCell.swift
+//  CRTimePicker
+//
+//  Created by CruzDiary on 25/03/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+class CRMinuteCell: UITableViewCell {
+
+    @IBOutlet weak var minuteLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
